0d45489c3e1ac11123274d0f0b4fddc013a437d2
Eugene Pavlovskiy <infopavlovskiy@gmail.com>
2022-08-06 01:19:25 +0200

    hero+header

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,28 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 import React, {useState} from "react"
+import logo from "./img/bmw-logo.png"
 
 export default function Navbar() {
   const [click, setClick] = useState(false)
 
   const handleClick = () => setClick(!click)
   return (
-    <nav className="nav">
-      <Link to="/" className="site-title" onClick={handleClick}>
-        BMW
-      </Link>
-      <ul className={click ? "nav-list active" : "nav-list"}>
-        <CustomLink to="/pricing" className="nav-link" onClick={handleClick}>Pricing</CustomLink>
-        <CustomLink to="/about" className="nav-link" onClick={handleClick}>About</CustomLink>
-      </ul>
-      
-      <div className="nav-icon" onClick={handleClick}>
-        <i className={click ? "fas fa-times":"fas fa-bars"}></i>
-      </div>
-    </nav>
+    <header>
+      <nav className="nav">
+        <Link to="/" className="site-title" onClick={handleClick}>
+          <img src={logo} className="nav-logo" alt="logo" />
+        </Link>
+        <ul className={click ? "nav-list active" : "nav-list"}>
+          <CustomLink to="/pricing" className="nav-link" onClick={handleClick}>Pricing</CustomLink>
+          <CustomLink to="/about" className="nav-link" onClick={handleClick}>About</CustomLink>
+        </ul>
+        
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times":"fas fa-bars"}></i>
+        </div>
+      </nav>
+    </header>
+    
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,20 @@
 export default function Home() {
-  return <div className="hero"><h1>Home</h1></div>
+  return (
+    
+    <div id="main">
+      <div className="hero">
+        <div className="hero-l">
+          <h1>THE NEW 3</h1>
+          <h2> DER BMW 3er TOURING: FÜR 714,- EURO IM MONAT LEASEN.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, minus
+            laudantium excepturi incidunt voluptate impedit quisquam. Dicta sed, ea
+            perferendis consequuntur expedita nesciunt nam quae omnis voluptatibus
+            corporis.
+          </p>
+        </div>
+        <div className="hero-r"></div>
+      </div>
+    </div>
+  );
 }
